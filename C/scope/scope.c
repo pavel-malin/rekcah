0255816a4b97682e66e597c812330e618285e9e7
pavel-malin <kurchevskijpavel@gmail.com>
2022-02-19 11:31:43 +0300

    variable scope

---
+++ C/scope/scope.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void func3()
+{
+	int i = 11;
+	printf("\t\t\t[we in func3] i = %d\n", i);
+}
+
+void func2()
+{
+	int i = 7;
+	printf("\t\t[we in func2] i = %d\n", i);
+	func3();
+	printf("\t[back to func2] i = %d\n", i);
+}
+
+void func1()
+{
+	int i = 5;
+	printf("\t[we in func1] i = %d\n", i);
+	func2();
+	printf("\t[back to func1] i = %d\n", i);
+}
+
+int main()
+{
+	int i = 3;
+	printf("[we in main] i = %d\n", i);
+	func1();
+	printf("[back to main] i = %d\n", i);
+}
